0808959cd5b38828ffcec7a3a14ff270be79d3f4
Frenzoid <frenzoid@protonmail.com>
2022-05-14 15:43:33 +0200

    detec when network changes on moodDiary

--- freshman/Dapp/frontend/index.js
+++ freshman/Dapp/frontend/index.js
@@ -1,16 +1,25 @@
-// Obtain provider
-const provider = new ethers.providers.Web3Provider(window.ethereum, "ropsten");
-
 // global variables.
+let provider;
 let moodContract;
+
 
 // On page load, we connect to the wallet.
 window.addEventListener("load", connectWallet);
 
+
 // We create an async function so we can use await, instead of .thens, because .thens are ugly af.
 async function connectWallet() {
+
+    // We connect to the wallet.
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // Get the user's wallet.
     await provider.send("eth_requestAccounts");
 
+    // Check the network.
+    await checkNetwork();
+
+    // Get the contract.
     const signer = provider.getSigner();
     moodContract = new ethers.Contract(moodDiaryMeta.output.address, moodDiaryMeta.output.abi, signer);
 
@@ -23,6 +32,12 @@ window.ethereum.on('accountsChanged', async () => {
     moodContract = new ethers.Contract(moodDiaryMeta.output.address, moodDiaryMeta.output.abi, signer);
 });
 
+window.ethereum.on('networkChanged', async () => {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    await checkNetwork();
+});
+
+
 // Main button functions
 async function getMood() {
     const mood = await moodContract.getMood();
@@ -31,5 +46,15 @@ async function getMood() {
 
 async function setMood() {
     const mood = document.getElementById("set_mood").value;
-    moodContract.setMood(mood);
+    const tx = await moodContract.setMood(mood);
+    await tx.wait();
+    getMood();
+}
+
+
+// Helper function
+async function checkNetwork() {
+    const { name } = await provider.getNetwork();
+    console.log(name);
+    if (name != "ropsten") alert("Please connect to the Ropsten test network.");
 }
